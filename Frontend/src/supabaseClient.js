b71631c1c8fc3ffd20641830afcbcc0634be63d3
Syed Mohammad Akhtar Rizvi <85864291+ShazanRizvi@users.noreply.github.com>
2024-05-30 04:28:42 +0530

    removed supabase config: frontend

--- Frontend/src/supabaseClient.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import { supabaseUrl, supabaseAnonKey } from './utils/constants'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
